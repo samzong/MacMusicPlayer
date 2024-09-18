23a61dfe08addf440627d98a6f064070fd5573a8
samzong.lu <samzong.lu@gmail.com>
2024-09-18 13:28:17 +0800

    Initial Commit

---
+++ MacMusicPlayer/MacMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMusicPlayerApp.swift
+//  MacMusicPlayer
+//
+//  Created by X on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MacMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
